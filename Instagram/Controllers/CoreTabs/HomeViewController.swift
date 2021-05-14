d2094e50679255847f5eac8c5ec8859fffddfcbb
Sivaranjani Venkatesh <sivaranjanivenkatesh@192-168-1-100.tpgi.com.au>
2021-05-14 23:34:43 +1000

    First Commit

---
+++ Instagram/Controllers/CoreTabs/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Sivaranjani Venkatesh on 11/5/21.
+//  Copyright © 2021 Sivaranjani Venkatesh. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
